ea4f7ca2a682ce5fa9ce9bab1e2870d7e4e957ce
jihun1233 <leejihun1233@gmail.com>
2022-05-27 18:48:22 +0900

    Update: 상세정보 카드 메시지와 점수 차이 가져오는 함수 추가

--- src/routes/OptionalMessage/index.tsx
+++ src/routes/OptionalMessage/index.tsx
@@ -5,28 +5,48 @@ import { getScoreDiffMessage } from 'utils/message'
 
 // MEMO: 메시지 테스트용 컴포넌트
 const OptionalMessage = (): JSX.Element => {
-  // TODO: json데이터로부터 얻은 점수로 메시지 출력하기
-  const diffScoreLastYear = 0
-  const diffScoreMaleAverage = 0
-  const diffScoreAfterTenYear = 0
-  const diffCostAfterTenYear = 0
+  // TODO: 상세결과 카드에 표시될 데이터 가져오기
+
+  /**
+   * MEMO:
+   * resBMI : 체질량지수
+   * resBolldPressure : 혈압
+   * resTotalColesterol : 총콜레스테롤
+   * smkOty : 흡연
+   * resFastingBloodSuger: 식전혈당
+   * drnkOty : 음주
+   * exerciQty : 운동량
+   * resGFR : 신사구체여과율
+   */
+
+  type Category =
+    | 'resBMI'
+    | 'resBloodPressure'
+    | 'resTotalCholesterol'
+    | 'smkQty'
+    | 'resFastingBloodSuger'
+    | 'drnkQty'
+    | 'exerciQty'
+    | 'resGFR'
+
+  const getHealthStatusMessage = (category: Category) => {
+    const { boj } = data.wxcResultMap
+    const status = boj[category].split(' - ')
+    const statusName = status[0]
+    const comments = status[1]
+    return { statusName, comments }
+  }
 
   useEffect(() => {
-    console.log(getScoreDiffMessage('lastYear', 1))
-    console.log(getScoreDiffMessage('lastYear', 0))
-    console.log(getScoreDiffMessage('lastYear', -1))
-    console.log(getScoreDiffMessage('maleAverage', 1))
-    console.log(getScoreDiffMessage('maleAverage', 0))
-    console.log(getScoreDiffMessage('maleAverage', -1))
-    console.log(getScoreDiffMessage('scoreAfterTenYear', 1))
-    console.log(getScoreDiffMessage('scoreAfterTenYear', 0))
-    console.log(getScoreDiffMessage('scoreAfterTenYear', -1))
-    console.log(getScoreDiffMessage('costAfterTenYear', 1))
-    console.log(getScoreDiffMessage('costAfterTenYear', 0))
-    console.log(getScoreDiffMessage('costAfterTenYear', -1))
+    console.log(getHealthStatusMessage('resBMI'))
   }, [])
 
-  return <div>항목별 건강 점수 조건에 따른 메시지</div>
+  return (
+    <div>
+      항목별 건강 점수 조건에 따른 메시지
+      <p>{getHealthStatusMessage('resBMI').statusName}</p>
+    </div>
+  )
 }
 
 export default OptionalMessage
--- src/services/health.ts
+++ src/services/health.ts
@@ -1,1 +1,25 @@
+import data from 'assets/jsons/user_info.json'
+
+// TODO: json파일에서 점수 데이터 가져와서 차이 계산
+const getScoreDiff = () => {
+  const { wxcResultMap, healthScoreList: yearData } = data
+  const { wHscore, medi, hscore_peer: hscorePeer, wHscoreDy, mediDy } = wxcResultMap
+  const myScore = Number(wHscore)
+  const currentCost = Number(medi)
+  const groupAverage = Number(hscorePeer)
+  const expectScoreAfterTenYears = Number(wHscoreDy)
+  const expectCostAfterTenYears = Number(mediDy)
+
+  const currentYear = new Date().getFullYear()
+  const scoreLastYear = Number(yearData.find((d) => currentYear - 1 === Number(d.SUBMIT_DATE.slice(0, 5)))?.SCORE)
+  // const scoreCurrentYear = yearData.find((d) => currentYear === Number(d.SUBMIT_DATE.slice(0, 5)))?.SCORE
+
+  const diffScoreLastYear = myScore - scoreLastYear
+  const diffScoreGroupAverage = myScore - groupAverage
+  const diffScoreAfterTenYear = myScore - expectScoreAfterTenYears
+  const diffCostAfterTenYear = currentCost - expectCostAfterTenYears
+
+  return {}
+}
+
 export {}
