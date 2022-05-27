cfef715f9f5b59562125bbc8710e8092d7a47022
Seohyun Roh <radult951@gmail.com>
2022-05-27 16:54:26 +0900

    ADD: UserInfo 레이아웃

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as LogoImage } from './logo.svg'
+
+export { ReactComponent as HealthInfo } from './healthInfo/ic-icon-health-info.svg'
+export { ReactComponent as HealthInfoArrow } from './healthInfo/ic-icon-health-info-arrow.svg'
