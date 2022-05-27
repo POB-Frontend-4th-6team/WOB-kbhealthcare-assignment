b0b488213ee4e05c65445b8dcbdead7afad61c02
jsg0629 <tnrms513@gmail.com>
2022-05-28 02:03:10 +0900

    feat: service 데이터와 dummyHealthManage 컴포넌트 동기화

--- src/routes/MainPage/_components/HealthManageCard/index.tsx
+++ src/routes/MainPage/_components/HealthManageCard/index.tsx
@@ -27,73 +27,62 @@ const HealthManageCard = ({
   const processHealthManageCardData = (_title: string) => {
     let title = ''
     let value = ''
-    let detail = ''
+    let isSmkDrnkExerciseDetail: string | boolean = false
     let normalOfStandard = ''
     const { tag } = healthManageCardData
     const boj = healthManageCardData.boj.slice(1)
     let signatureColor = ''
-    let svg: any = null // svg element타입  SVGElement 아닌가요,,? SVGElement | null 넣으니 에러가나네요,,
+    let svg: JSX.Element | null = null
 
     if (_title === 'resBMI') {
-      title = '체질량 지수'
+      title = '체질량 지수는'
       value = `${healthManageCardData.value}kg/㎡`
-      detail = `체질량 지수는 ${healthManageCardData.value}kg/㎡로 ${healthManageCardData.boj[0]} 입니다.`
       normalOfStandard = '정상 : 18.5 ~ 22.9 kg/㎡'
       signatureColor = '#7bcf3d'
       svg = <BMIIcon />
     }
     if (_title === 'resBloodPressure') {
-      title = '혈압'
+      title = '혈압은'
       value = `${healthManageCardData.value}mmHg`
-      detail = `혈압은 ${healthManageCardData.value}mmHg로 ${healthManageCardData.boj[0]} 입니다.`
       normalOfStandard = '정상 : 이완 60~79 / 수축 90~119 mmHg'
       signatureColor = '#ca64e8'
       svg = <BloodPressureIcon />
     }
     if (_title === 'resTotalCholesterol') {
-      title = '총콜레스테롤'
+      title = '총콜레스테롤은'
       value = `${healthManageCardData.value}mg/dL`
-      detail = `총콜레스테롤은 ${healthManageCardData.value}mg/dL로 ${healthManageCardData.boj[0]} 입니다.`
       normalOfStandard = '정상 : 200 mg/dL 이하'
       signatureColor = '#3f60f0'
       svg = <CholesterolIcon />
     }
     if (_title === 'smkQty') {
       title = '흡연'
-      value = ''
-      detail = `${healthManageCardData.boj[0]} 중 입니다`
-      normalOfStandard = ''
+      isSmkDrnkExerciseDetail = `${healthManageCardData.boj[0]}`
       signatureColor = '#68a0eb'
       svg = <SmokeIcon />
     }
     if (_title === 'resFastingBloodSuger') {
-      title = '식전혈당'
+      title = '식전혈당은'
       value = `${healthManageCardData.value}mg/dL`
-      detail = `식전혈당은 ${healthManageCardData.value}mg/dL로 ${healthManageCardData.boj[0]} 입니다.`
       normalOfStandard = '정상 : 69~99 mg/dL'
       signatureColor = '#d990f0'
       svg = <FastingBloodSugerIcon />
     }
     if (_title === 'drnkQty') {
       title = '음주'
-      value = ``
-      detail = `${healthManageCardData.boj[0]} 입니다.`
-      normalOfStandard = ''
+      isSmkDrnkExerciseDetail = `${healthManageCardData.boj[0]}`
       signatureColor = '#e99318'
       svg = <DrnkIcon />
     }
     if (_title === 'exerciQty') {
       title = '운동량'
-      value = ``
-      detail = `${healthManageCardData.boj[0]} 을 하고 있습니다.`
-      normalOfStandard = ''
+      isSmkDrnkExerciseDetail = `${healthManageCardData.boj[0]}`
       signatureColor = '#73ccbc'
       svg = <ExerciseIcon />
     }
     if (_title === 'resGFR') {
-      title = '신사구체여과율'
+      title = '신사구체여과율은'
       value = `${healthManageCardData.value}mL/min `
-      detail = `신사구체여과율은 ${healthManageCardData.value}mL/min로 ${healthManageCardData.boj[0]} 입니다.`
       normalOfStandard = '정상 : 60 mL/min 이상'
       signatureColor = '#edb3b4'
       svg = <GFRIcon />
@@ -102,7 +91,7 @@ const HealthManageCard = ({
     return {
       title,
       value,
-      detail,
+      isSmkDrnkExerciseDetail,
       normalOfStandard,
       tag,
       boj,
@@ -111,9 +100,8 @@ const HealthManageCard = ({
     }
   }
 
-  const { title, detail, normalOfStandard, tag, boj, signatureColor, svg } = processHealthManageCardData(
-    healthManageCardData.title
-  )
+  const { title, value, isSmkDrnkExerciseDetail, normalOfStandard, tag, boj, signatureColor, svg } =
+    processHealthManageCardData(healthManageCardData.title)
 
   return (
     <ul>
@@ -124,11 +112,21 @@ const HealthManageCard = ({
           <p className={styles.healthTitle} style={{ color: signatureColor }}>
             {title}
           </p>
-          <p className={styles.detail}>{detail}</p>
+          <p className={styles.detail}>
+            {isSmkDrnkExerciseDetail || (
+              <>
+                {title} {value}로 <br />
+                <mark>{healthManageCardData.boj[0]}</mark> 입니다.
+              </>
+            )}
+          </p>
           <p className={styles.normal}>{normalOfStandard}</p>
           <ul className={styles.tag}>
             {tag.map((el) => {
-              return <li key={`key_${el}`}>#{el}</li>
+              if (el) {
+                return <li key={`key_${el}`}>#{el}</li>
+              }
+              return undefined
             })}
           </ul>
         </div>
