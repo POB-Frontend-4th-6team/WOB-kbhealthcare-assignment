07b0ac329b83bfee35ee66f3604b9cf75745ace9
jihun1233 <leejihun1233@gmail.com>
2022-05-27 18:47:55 +0900

    Update: 용어 male -> group으로 변경

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,4 +1,4 @@
-type CompareTarget = 'lastYear' | 'maleAverage' | 'scoreAfterTenYear' | 'costAfterTenYear'
+type CompareTarget = 'lastYear' | 'groupAverage' | 'scoreAfterTenYears' | 'costAfterTenYears'
 
 type IWords = {
   [key in CompareTarget]: { unit: string; increased: string; decreased: string }
@@ -6,9 +6,9 @@ type IWords = {
 
 const words: IWords = {
   lastYear: { unit: '점', increased: '높아졌어요', decreased: '낮아졌어요' },
-  maleAverage: { unit: '점', increased: '높아요', decreased: '낮아요' },
-  scoreAfterTenYear: { unit: '점', increased: '높아요', decreased: '낮아요' },
-  costAfterTenYear: { unit: '원', increased: '높아요', decreased: '낮아요' },
+  groupAverage: { unit: '점', increased: '높아요', decreased: '낮아요' },
+  scoreAfterTenYears: { unit: '점', increased: '높아요', decreased: '낮아요' },
+  costAfterTenYears: { unit: '원', increased: '높아요', decreased: '낮아요' },
 }
 
 export const getScoreDiffMessage = (compareTarget: CompareTarget, score: number) => {
