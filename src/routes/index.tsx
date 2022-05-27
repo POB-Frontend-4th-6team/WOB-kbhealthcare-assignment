ffac0258dd8f2bbb81a7c1b39c66ae6c88bae984
jihun1233 <leejihun1233@gmail.com>
2022-05-27 20:58:13 +0900

    Delete: 테스트용으로 사용하던 컴포넌트와 라우트 삭제

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import styles from './routes.module.scss'
 import LoginPage from './LoginPage'
 import MainPage from './MainPage'
-import OptionalMessage from './OptionalMessage'
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
         <Routes>
           <Route path='/' element={<MainPage />} />
           <Route path='login' element={<LoginPage />} />
-          <Route path='optionalMessage' element={<OptionalMessage />} />
           <Route path='*' element={<div>404</div>} />
         </Routes>
       </main>
