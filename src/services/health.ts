9ffa0542f6b4fe114a1cbe601d6d546b02768863
jihun1233 <leejihun1233@gmail.com>
2022-05-27 20:52:26 +0900

    Update: 오류 있던 부분 수정, 함수 분리
    - getScoreDiffMessageAll을 통해 모든 메시지를 반환하고 이 안에서 데이터를 가져오는 함수를 getScoreDatas로 분리
    
    - 10년 이후의 점수와 비용을 계산할 때는 기준이 바뀌어 10년 후의 비용에 현재 비용을 빼야해서 수정
    - slice를 잘못사용하여 연도별점수에서 연도가 5자리로 나와 4자리로 받도록 두번째 인자 4로 수정
    - 10년후 비용을 계산할 때 10년간의 데이터가 배열이 아닌 배열의 스트링형태로 나타나있어 이를 배열로 바꾼 후 마지막값을 pop하여 사용
    - 그룹 내에서 상위 퍼센트를 구해서 이것도 메세지로 만들어 반환
    - 연도별 건강점수가 없거나 하나만 있을 경우를 나눠 각각 다른 메시지를 주도록 함

--- src/services/health.ts
+++ src/services/health.ts
@@ -1,25 +1,61 @@
 import data from 'assets/jsons/user_info.json'
+import { getScoreDiffMessage } from 'utils/message'
 
-// TODO: json파일에서 점수 데이터 가져와서 차이 계산
-const getScoreDiff = () => {
+const getScoreDatas = () => {
   const { wxcResultMap, healthScoreList: yearData } = data
-  const { wHscore, medi, hscore_peer: hscorePeer, wHscoreDy, mediDy } = wxcResultMap
+  const { wHscore, medi, hscore_peer: hscorePeer, wHscoreDy, mediDy, hscorePercent } = wxcResultMap
   const myScore = Number(wHscore)
   const currentCost = Number(medi)
   const groupAverage = Number(hscorePeer)
-  const expectScoreAfterTenYears = Number(wHscoreDy)
-  const expectCostAfterTenYears = Number(mediDy)
+  const expectScoreAfterTenYears = Number(JSON.parse(wHscoreDy).pop())
+  const expectCostAfterTenYears = Number(JSON.parse(mediDy).pop())
+  const myScoreGroupPercent = 100 - Number(hscorePercent)
 
   const currentYear = new Date().getFullYear()
-  const scoreLastYear = Number(yearData.find((d) => currentYear - 1 === Number(d.SUBMIT_DATE.slice(0, 5)))?.SCORE)
-  // const scoreCurrentYear = yearData.find((d) => currentYear === Number(d.SUBMIT_DATE.slice(0, 5)))?.SCORE
+  const scoreLastYear = Number(yearData.find((d) => currentYear - 1 === Number(d.SUBMIT_DATE.slice(0, 4)))?.SCORE)
 
   const diffScoreLastYear = myScore - scoreLastYear
   const diffScoreGroupAverage = myScore - groupAverage
-  const diffScoreAfterTenYear = myScore - expectScoreAfterTenYears
-  const diffCostAfterTenYear = currentCost - expectCostAfterTenYears
+  const diffScoreAfterTenYear = expectScoreAfterTenYears - myScore
+  const diffCostAfterTenYear = expectCostAfterTenYears - currentCost
 
-  return {}
+  return {
+    diffScoreLastYear,
+    diffScoreGroupAverage,
+    diffScoreAfterTenYear,
+    diffCostAfterTenYear,
+    yearData,
+    myScoreGroupPercent,
+  }
 }
 
-export {}
+const getScoreDiffMessageAll = () => {
+  const {
+    diffScoreLastYear,
+    diffScoreGroupAverage,
+    diffScoreAfterTenYear,
+    diffCostAfterTenYear,
+    yearData,
+    myScoreGroupPercent,
+  } = getScoreDatas()
+
+  let diffScoreLastYearMessage = getScoreDiffMessage('lastYear', diffScoreLastYear)
+  const diffScoreGroupAverageMessage = getScoreDiffMessage('groupAverage', diffScoreGroupAverage)
+  const diffScoreAfterTenYearMessage = getScoreDiffMessage('scoreAfterTenYears', diffScoreAfterTenYear)
+  const diffCostAfterTenYearMessage = getScoreDiffMessage('costAfterTenYears', diffCostAfterTenYear)
+  const myScoreGroupPercentMessage = `상위 ${myScoreGroupPercent}%`
+
+  if (yearData.length === 1)
+    diffScoreLastYearMessage = `${yearData[0].SUBMIT_DATE.slice(0, 4)}년 건강 점수는 ${yearData[0].SCORE}점 입니다`
+  if (yearData.length === 0) diffScoreLastYearMessage = '연도별 건강 점수가 아직 없습니다'
+
+  return {
+    diffScoreLastYearMessage,
+    diffScoreGroupAverageMessage,
+    diffScoreAfterTenYearMessage,
+    diffCostAfterTenYearMessage,
+    myScoreGroupPercentMessage,
+  }
+}
+
+export { getScoreDiffMessageAll }
