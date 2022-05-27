a959350b1ed552137715bb7f5f7c79eaf5121deb
in3166 <in3166@naver.com>
2022-05-27 22:05:59 +0900

    ADD: HealthManage Data 가져오는 함수 추가

--- src/services/health.ts
+++ src/services/health.ts
@@ -1,1 +1,48 @@
-export {}
+import data from 'assets/jsons/user_info.json'
+
+interface IHealthManageObject {
+  [key: string]: string
+}
+
+const getHealthManageData = () => {
+  const property = [
+    'resBMI',
+    'resBloodPressure',
+    'resTotalCholesterol',
+    'smkQty',
+    'resFastingBloodSuger',
+    'drnkQty',
+    'resGFR',
+    'exerciQty',
+  ]
+
+  const { healthTagList, userInfo, wxcResultMap } = data
+
+  const userScore = Number(userInfo.healthScore)
+  const wMymaxHscoreDy = JSON.parse(wxcResultMap.wMymaxHscoreDy).filter((val: number) => val > userScore)
+
+  const { boj, paramMap }: { boj: IHealthManageObject; paramMap: IHealthManageObject } = wxcResultMap
+
+  const covertedHealthManageData = property.map((value) => {
+    const tag: string[] = []
+    healthTagList.forEach((currentValue) => {
+      if (currentValue.tagId === value) tag.push(currentValue.tag1, currentValue.tag2, currentValue.tag3)
+    })
+
+    const splitedBoj = boj[value].split('-').map((element) => element.trim())
+
+    return {
+      title: value,
+      value: paramMap[value],
+      boj: splitedBoj,
+      tag,
+    }
+  })
+
+  return {
+    wMymaxHscoreDy,
+    healthMangeCardData: covertedHealthManageData,
+  }
+}
+
+export { getHealthManageData }
