04199631e98518574c4fd6ad66b28a8309a690d1
markyul <pig68111000@gmail.com>
2022-05-27 22:03:32 +0900

    Feature: 연도 별 그래프 정적 데이터로 구현

--- src/routes/MainPage/HealthCharts/index.tsx
+++ src/routes/MainPage/HealthCharts/index.tsx
@@ -1,9 +1,11 @@
 import styles from './healthCharts.module.scss'
+import YearsChart from './YearsChart'
 
 const HealthCharts = () => {
   return (
     <section className={styles.container}>
       <h2>HealthCharts</h2>
+      <YearsChart />
     </section>
   )
 }
