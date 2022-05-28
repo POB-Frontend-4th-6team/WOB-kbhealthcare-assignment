bcb4e968f423120abd54630c325cf1d15cb7bb35
Ji Hyuk Ma <t90laser93@gmail.com>
2022-05-28 21:47:56 +0900

    DEL: 주석처리된 코드 정리

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,7 +17,6 @@ const App = () => {
           <Route path='*' element={<div>404</div>} />
           <Route path='login' element={<LoginPage />} />
         </Routes>
-        {/* <CustomRoutes /> */}
       </main>
     </div>
   )
