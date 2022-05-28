a2a218e3582bfec75a9f4a668099b1a27714e2b2
Jihyuk Ma <t90laser93@gmail.com>
2022-05-28 15:50:09 +0900

    feat: adding login page
    -

--- src/routes/LoginPage/index.tsx
+++ src/routes/LoginPage/index.tsx
@@ -1,7 +1,19 @@
 import styles from './loginPage.module.scss'
 
 const LoginPage = () => {
-  return <div className={styles.container}>Login</div>
+  const handleLoginOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.loginForm} onSubmit={handleLoginOnSubmit}>
+        <label htmlFor='idInput'>UserID</label>
+        <input id='idInput' type='text' />
+      </form>
+      <div className={styles.logoContainer} />
+    </div>
+  )
 }
 
 export default LoginPage
