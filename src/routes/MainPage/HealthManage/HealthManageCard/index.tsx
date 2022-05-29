eb3ddbcaca94e967c2bc32fc96835525775ab478
rkddbs1031 <rkddbs1031@gmail.com>
2022-05-29 13:47:46 +0900

    FIX: HealthMange, HealthManageCard 컴포넌트 ul태그 수정

--- src/routes/MainPage/HealthManage/HealthManageCard/index.tsx
+++ src/routes/MainPage/HealthManage/HealthManageCard/index.tsx
@@ -114,42 +114,40 @@ const HealthManageCard = ({
   )
 
   return (
-    <ul>
-      <li className={styles.healthCard}>
-        <div className={styles.icon}>{svg}</div>
-        <div className={styles.info}>
-          <h3>0{idx}</h3>
-          <p className={styles.healthTitle} style={{ color: signatureColor }}>
-            {title}
-          </p>
-          <p className={styles.detail}>
-            {isSmkDrnkExerciseDetail || (
-              <>
-                {title} {value}로 <br />
-                <mark>{healthManageCardData.boj[0]}</mark> 입니다.
-              </>
-            )}
-          </p>
-          <p className={styles.normal}>{standardOfNormal}</p>
-          <ul className={styles.tag}>
-            {tag.map((el) => {
-              if (el) {
-                return <li key={`key_${el}`}>#{el}</li>
-              }
-              return null
-            })}
-          </ul>
-        </div>
-        <div className={styles.manage}>
-          <h4 style={{ color: signatureColor }}>이렇게 관리해 보세요!</h4>
-          <ul>
-            {boj.map((el) => {
-              return <li key={`key_${el}`}>{el}</li>
-            })}
-          </ul>
-        </div>
-      </li>
-    </ul>
+    <li className={styles.healthCard}>
+      <div className={styles.icon}>{svg}</div>
+      <div className={styles.info}>
+        <h3>0{idx}</h3>
+        <p className={styles.healthTitle} style={{ color: signatureColor }}>
+          {title}
+        </p>
+        <p className={styles.detail}>
+          {isSmkDrnkExerciseDetail || (
+            <>
+              {title} {value}로 <br />
+              <mark>{healthManageCardData.boj[0]}</mark> 입니다.
+            </>
+          )}
+        </p>
+        <p className={styles.normal}>{standardOfNormal}</p>
+        <ul className={styles.tag}>
+          {tag.map((el) => {
+            if (el) {
+              return <li key={`key_${el}`}>#{el}</li>
+            }
+            return null
+          })}
+        </ul>
+      </div>
+      <div className={styles.manage}>
+        <h4 style={{ color: signatureColor }}>이렇게 관리해 보세요!</h4>
+        <ul>
+          {boj.map((el) => {
+            return <li key={`key_${el}`}>{el}</li>
+          })}
+        </ul>
+      </div>
+    </li>
   )
 }
 
